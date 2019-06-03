8cffc96d1e33cfeffd6624bd719945ad340e024b
abarglowski <barglowskidrew@gmail.com>
2019-06-03 12:02:10 -0400

    saving work

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,20 @@ import Welcome from "./pages/Welcome"
 import SignUp from "./pages/SignUp"
 import Login from "./components/Login"
 import CreateList from "./pages/CreateList"
+import RegistrySearch from "./pages/RegistrySearch"
 
 
 function App() {
   return (
      <Router>
+       <Navbar/>
       <React.Fragment>
         <Switch>
           <Route exact path="/" component={Welcome} />
           <Route exact path="/SignUp" component={SignUp} />
-          <Route exact path="Login" component={Login} />
-          <Route exact path="CreateList" component={CreateList} />
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/CreateList" component={CreateList} />
+          <Route exact path="/RegistrySearch" component={RegistrySearch} />
         </Switch>
       </React.Fragment>
      </Router>
